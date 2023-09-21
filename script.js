cf1a9cd2e982d7e2beaddb671f280cd4f94e77ea
Julio Murillo <julioamurillo10@gmail.com>
2023-09-20 20:56:29 -0500

    cambios bard

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-// script.js
+// Variables
+let timer = 0;
 let running = false;
-let startTime = null;
-let interval;
 
-const display = document.getElementById("display");
-const startStopButton = document.getElementById("startStop");
-const resetButton = document.getElementById("reset");
-
-function resetTimer() {
-    clearInterval(interval);
-    running = false;
-    startStopButton.textContent = "Iniciar";
-    startStopButton.classList.remove("running");
-    display.textContent = "00:00:00.000";
-    startTime = null;
+// Función para iniciar el cronómetro
+function start() {
+  running = true;
+  updateTimer();
 }
 
-resetButton.addEventListener("click", () => {
-    resetTimer();
-});
-
-startStopButton.addEventListener("click", () => {
-    if (!running) {
-        if (startTime === null) {
-            startTime = Date.now();
-        }
-        startStopButton.textContent = "Detener";
-        startStopButton.classList.add("running");
-        interval = setInterval(updateDisplay, 10);
-    } else {
-        clearInterval(interval);
-    }
-    running = !running;
-});
-
-function updateDisplay() {
-    const currentTime = new Date(Date.now() - startTime);
-    const hours = String(currentTime.getUTCHours()).padStart(2, "0");
-    const minutes = String(currentTime.getUTCMinutes()).padStart(2, "0");
-    const seconds = String(currentTime.getUTCSeconds()).padStart(2, "0");
-    const milliseconds = String(currentTime.getUTCMilliseconds()).padStart(3, "0");
-    display.textContent = `${hours}:${minutes}:${seconds}.${milliseconds}`;
+// Función para pausar el cronómetro
+function pause() {
+  running = false;
 }
+
+// Función para reiniciar el cronómetro
+function reset() {
+  timer = 0;
+  updateTimer();
+}
+
+// Función para actualizar el tiempo del cronómetro
+function updateTimer() {
+  let minutes = Math.floor(timer / 60);
+  let seconds = Math.floor((timer % 60) / 10);
+  let milliseconds = timer % 10;
+
+  // Actualizamos la interfaz de usuario
+  document.querySelector(".timer h1").textContent = `${minutes}:${seconds}:${milliseconds}`;
+
+  // Si el cronómetro está en marcha, actualizamos el tiempo cada 100 milisegundos
+  if (running) {
+    setTimeout(updateTimer, 100);
+  }
+}
+
+// Eventos
+document.querySelector("#start").addEventListener("click", start);
+document.querySelector("#pause").addEventListener("click", pause);
+document.querySelector("#reset").addEventListener("click", reset);
+
+// Iniciamos el cronómetro
+updateTimer();
