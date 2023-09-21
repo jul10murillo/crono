04884ae103278df8a7c252a6874eedcd623c1e19
Julio Murillo <julioamurillo10@gmail.com>
2023-09-20 21:39:38 -0500

    con los tiempos

--- script.js
+++ script.js
@@ -2,17 +2,24 @@
 let running = false;
 let startTime = null;
 let interval;
+let selectedTime = 0; // Variable para almacenar el tiempo seleccionado
 
 const display = document.getElementById("display");
 const startStopButton = document.getElementById("startStop");
 const resetButton = document.getElementById("reset");
+const tiempoSelect = document.getElementById("tiempo");
+
+tiempoSelect.addEventListener("change", () => {
+    selectedTime = parseInt(tiempoSelect.value);
+    resetTimer();
+});
 
 function resetTimer() {
     clearInterval(interval);
     running = false;
     startStopButton.textContent = "Iniciar";
     startStopButton.classList.remove("running");
-    display.textContent = "00:00:00.000";
+    display.textContent = formatTime(selectedTime); // Mostrar el tiempo seleccionado
     startTime = null;
 }
 
@@ -23,7 +30,7 @@ resetButton.addEventListener("click", () => {
 startStopButton.addEventListener("click", () => {
     if (!running) {
         if (startTime === null) {
-            startTime = Date.now();
+            startTime = Date.now() - (selectedTime * 1000); // Iniciar desde el tiempo seleccionado
         }
         startStopButton.textContent = "Detener";
         startStopButton.classList.add("running");
@@ -36,9 +43,24 @@ startStopButton.addEventListener("click", () => {
 
 function updateDisplay() {
     const currentTime = new Date(Date.now() - startTime);
-    const hours = String(currentTime.getUTCHours()).padStart(2, "0");
-    const minutes = String(currentTime.getUTCMinutes()).padStart(2, "0");
-    const seconds = String(currentTime.getUTCSeconds()).padStart(2, "0");
-    const milliseconds = String(currentTime.getUTCMilliseconds()).padStart(3, "0");
-    display.textContent = `${hours}:${minutes}:${seconds}.${milliseconds}`;
+    const timeRemaining = Math.max(selectedTime * 1000 - currentTime.getTime(), 0);
+
+    if (timeRemaining === 0) {
+        // Mostrar mensaje al finalizar el tiempo
+        display.textContent = "Tiempo terminado";
+        clearInterval(interval);
+    } else {
+        display.textContent = formatTime(timeRemaining);
+    }
 }
+
+function formatTime(time) {
+    const date = new Date(time);
+    const hours = String(date.getUTCHours()).padStart(2, "0");
+    const minutes = String(date.getUTCMinutes()).padStart(2, "0");
+    const seconds = String(date.getUTCSeconds()).padStart(2, "0");
+    const milliseconds = String(date.getUTCMilliseconds()).padStart(3, "0");
+    return `${hours}:${minutes}:${seconds}.${milliseconds}`;
+}
+
+resetTimer(); // Iniciar con el tiempo seleccionado por defecto
