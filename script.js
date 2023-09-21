9ef0ee83454d3e17d39459835178577ef477c380
Julio Murillo <julioamurillo10@gmail.com>
2023-09-20 20:50:24 -0500

    nuevos cambios

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // script.js
 let running = false;
 let startTime = null;
-let interval;
+let elapsedTime = 0; // Nuevo
 
 const display = document.getElementById("display");
 const startStopButton = document.getElementById("startStop");
@@ -14,6 +14,7 @@ function resetTimer() {
     startStopButton.classList.remove("running");
     display.textContent = "00:00:00.000";
     startTime = null;
+    elapsedTime = 0; // Reiniciar el tiempo transcurrido
 }
 
 resetButton.addEventListener("click", () => {
@@ -23,19 +24,20 @@ resetButton.addEventListener("click", () => {
 startStopButton.addEventListener("click", () => {
     if (!running) {
         if (startTime === null) {
-            startTime = Date.now();
+            startTime = Date.now() - elapsedTime; // Restaurar el tiempo transcurrido
         }
         startStopButton.textContent = "Detener";
         startStopButton.classList.add("running");
         interval = setInterval(updateDisplay, 10);
     } else {
         clearInterval(interval);
+        elapsedTime = Date.now() - startTime; // Al detener, calcula el tiempo transcurrido
     }
     running = !running;
 });
 
 function updateDisplay() {
-    const currentTime = new Date(Date.now() - startTime);
+    const currentTime = new Date(elapsedTime);
     const hours = String(currentTime.getUTCHours()).padStart(2, "0");
     const minutes = String(currentTime.getUTCMinutes()).padStart(2, "0");
     const seconds = String(currentTime.getUTCSeconds()).padStart(2, "0");
